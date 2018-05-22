a753081b9c63549e014add06f92a4db04eafb895
Fred Bradley <hello@fredbradley.co.uk>
2018-05-22 13:54:39 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+/* Load .env */
+$dotenv = new \Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+$dotenv->required(['GOOGLE_CALENDAR_ICS_URI']);
+
+
+/* File path or url of the .ics document */
+$file = getenv("GOOGLE_CALENDAR_ICS_URI");
+
+
+/* Getting events from isc file */
+$calendar = new CranleighSchool\AnnualLeave\parseIcs($file);
+echo '<h3 class="widgettitle">Annual Leave</h3>';
+echo '<div class="leavedatablock">';
+echo $calendar->displayTable();
+echo '</div>';
